c9170b79a7ed4fcdda753d0a9eb0f89c993c6144
Abhishek <abhigeu9193@gmail.com>
2022-05-06 17:39:56 +0530

    NLoger Solution

---
+++ Nloger/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Nloger;
+
+Console.WriteLine("Hello World!");
+AddNumbers obj = new AddNumbers();
+Console.WriteLine("Result:- " + obj.Sum(1, 2));
